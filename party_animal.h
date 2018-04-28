5bd52c9e68dbe9314185fd063e6a679a2b5f2c09
Jared Frank <jfrank1120@verizon.net>
2018-04-28 12:21:49 -0400

    Commit 4/28, 12:21
    Finished all of Party Animal except for error with cout statement

--- party_animal.h
+++ party_animal.h
@@ -8,7 +8,17 @@ class party_animal : public werewolf
 };
 
 void party_animal::eat_person(person *pers) {
-	if (pers->has_item("beer") {
-
+	if (pers->has_item("beer")) {
+		thing *beer = pers->
+		cout << get_name() << "says OH! IS THAT A BEER!? ";
+		// Person loses Beer from their inventory
+		pers->drop(beer);
+		// Party_Animal takes beer
+		take(beer);
+		cout << get_name() << ": Takes your beer and proceeds to drink it" << endl;
+		cout << get_name() << ": Seems to be feeling woozey" << endl;
+	}
+	else {
+		werewolf::eat_person(pers);
 	}
 }
